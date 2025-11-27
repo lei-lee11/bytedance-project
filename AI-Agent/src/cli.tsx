53345c321005456040056321e3ebdb158fb08f09
AlanLee <1637295803@qq.com>
2025-11-27 18:45:33 +0800

    实现cli基本界面

---
+++ AI-Agent/src/cli.tsx
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+// src/cli.tsx
+import React from "react";
+import { render } from "ink";
+import { App } from "./ui/App.js"; // 注意后缀，ts-node 有时需要处理
+import minimist from "minimist";
+
+// 解析命令行参数
+const args = minimist(process.argv.slice(2));
+
+// 获取第一个非 flag 参数作为初始消息
+// 例如: npm start "帮我写个代码" -> initialMessage = "帮我写个代码"
+const initialMessage = args._[0] ? String(args._[0]) : undefined;
+
+// 渲染 UI
+// clear: true 会在退出时清除 UI，根据喜好设置
+render(<App initialMessage={initialMessage} />);
--- AI-Agent/src/config/model.ts
+++ AI-Agent/src/config/model.ts
@@ -3,7 +3,7 @@ import { tools } from "../utils/tools/index.ts";
 export const baseModel = new ChatOpenAI({
   model: "doubao-seed-1-6-251015",
   temperature: 0,
-  streaming: false,
+  //streaming: false,
   maxRetries: 3,
   timeout: 30000,
   apiKey: " fc223d35-9d61-483a-9d5b-7d319d2b7494",
